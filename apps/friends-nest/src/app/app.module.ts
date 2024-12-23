ce3b157230fd0edc9d77d2efadf56fa56ca0ac05
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-12-23 09:43:55 -0300

    chore: conexão com o banco de dados

--- apps/friends-nest/src/app/app.module.ts
+++ apps/friends-nest/src/app/app.module.ts
@@ -6,25 +6,26 @@ import { UserModule } from './user/user.module';
 
 //NOME DA INSTÂNCIA SQLEXPRESS01: Server=localhost\SQLEXPRESS01;Database=master;Trusted_Connection=True;
 // ADMINISTRADORES SQL WELLYSSON-ERGON\welly: C:\Program Files\Microsoft SQL Server\160\Setup Bootstrap\Log\20241218_130802
+// id DA INSTÂNCIA: MSSQL16.SQLEXPRESS01
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mssql',
-      host: 'WELLYSSON-ERGON', // ou o seu host correto
-      port: 1433,
-      username: 'sa',
-      password: 'Well#34125234', // senha que você configurou
-      database: 'friends',
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: true,
-      autoLoadEntities: true,
-      logging: true,
-      logger: 'file',
+      host: 'WELLYSSON-ERGON', // Nome do host ou máquina
+      port: 1433, // Porta padrão
+      username: 'sa', // Usuário de autenticação
+      password: 'Well#34125234',
+      database: 'friends', // Nome do banco de dados
       extra: {
-        trustServerCertificate: true, // Se necessário
-        encrypt: true, // Caso precise de criptografia
-        enableArithAbort: true, // Pode ser necessário dependendo da versão do SQL Server
+        trustServerCertificate: true, // Aceitar certificado SSL, se necessário
+        integratedSecurity: true, // Habilita autenticação integrada do Windows
       },
+      options: {
+        encrypt: false, // Ajuste para conexões seguras
+        instanceName: 'SQLEXPRESS', // Nome da instância do SQL Server
+      },
+      synchronize: true, // Apenas para desenvolvimento (gera automaticamente tabelas)
+      autoLoadEntities: true, // Carrega automaticamente as entidades
     }),
     UserModule,
   ],
